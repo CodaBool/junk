885c272474217eeff9ca2b5d86a75d1f48be3f2e
Coda Bool <CodaBool@pm.me>
2025-01-02 23:12:41 -0500

    rm log

--- app/page.js
+++ app/page.js
@@ -8,15 +8,8 @@ export default function Home() {
   useEffect(() => {
     const {hash} = window.location
     if (hash) {
-
-
-      console.log("raw hash", hash.slice(1))
-
       const params = new URLSearchParams(hash.slice(1));
       const accessToken = params.get("access_token");
-
-      console.log("accessToken", accessToken)
-
       fetch(`/api?access_token=${accessToken}`)
         .then((res) => res.json())
         .then((json) => {
