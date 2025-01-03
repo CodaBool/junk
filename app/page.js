cc4161d90a3f56486b1de9479f0e72f753fb77db
Coda Bool <CodaBool@pm.me>
2025-01-02 19:25:15 -0500

    equal test

--- app/page.js
+++ app/page.js
@@ -8,8 +8,16 @@ export default async function Home() {
   useEffect(() => {
     const {hash} = window.location
     if (hash) {
-      console.log("sending request with token", hash.slice(1))
-      fetch(`/api?access_token%3D${hash.slice(1)}`)
+
+
+      console.log("raw hash", hash.slice(1))
+
+      const params = new URLSearchParams(hash.slice(1));
+      const accessToken = params.get("access_token");
+
+      console.log("accessToken", accessToken)
+
+      fetch(`/api?access_token=${accessToken}`)
         .then((res) => res.json())
         .then((json) => console.log("result", json));
     }
