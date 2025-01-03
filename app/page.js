f4c65666264b5ee906d1709eb962bb997ec4db58
Coda Bool <CodaBool@pm.me>
2025-01-02 19:13:33 -0500

    put window into mount

--- app/page.js
+++ app/page.js
@@ -3,17 +3,16 @@ import { useEffect } from 'react'
 import '../styles/global.css'
 
 export default async function Home() {
-  const {hash} = window.location
-  if (!hash) {
-    console.log("no hash", window.location)
-    return (<h1>hash required</h1>)
-  }
+
 
   useEffect(() => {
-    console.log("sending request with token", hash.slice(1))
-    fetch(`/api?access_token=${hash.slice(1)}`)
-      .then((res) => res.json())
-      .then((json) => console.log("result", json));
+    const {hash} = window.location
+    if (hash) {
+      console.log("sending request with token", hash.slice(1))
+      fetch(`/api?access_token=${hash.slice(1)}`)
+        .then((res) => res.json())
+        .then((json) => console.log("result", json));
+    }
   }, [])
 
 
